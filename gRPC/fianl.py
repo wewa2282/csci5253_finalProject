8a4bb908cfb9e62890af16fb71fe57dfe06692c6
weiqiang-Wang <59746450+weiqiang-Wang@users.noreply.github.com>
2022-12-02 15:07:42 -0700

    gRPC templet

---
+++ gRPC/fianl.py
@@ -0,0 +1,64 @@
+import base64
+import io, os, sys
+from concurrent import futures
+import grpc
+import final_pb2
+import final_pb2_grpc
+from flask import Flask, request, Response,send_file
+import jsonpickle, pickle
+import platform
+import redis
+import hashlib, requests
+import base64
+from minio import Minio
+from  minio.deleteobjects import DeleteObject
+import hashlib
+
+redisHost =  os.getenv("REDIS_HOST") or "localhost"
+redisPort =  os.getenv("REDIS_PORT") or "6379"
+
+
+minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
+minioUser = os.getenv("MINIO_USER") or "rootuser"
+minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
+
+queueBucketName = "queue"
+outpuutBucketName = "output"
+
+workQueue = "toWorker"
+
+redisClient = redis.StrictRedis(host = redisHost,port= redisPort,db=0)
+minioClient = Minio(minioHost,
+               secure=False,
+               access_key=minioUser,
+               secret_key=minioPasswd )
+
+
+
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+class RouteGuideServicer(lab6_pb2_grpc.lab6Servicer):
+     def __init__(self) -> None:
+        pass
+
+     def doconvert(self,request,context):
+        pass
+     def queue(self,request,context):
+        pass
+     def delete(self,request,context):
+        pass
+
+def serve():    
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    final__pb2_grpc.add
+    final__pb2_grpc.add_lab6Servicer_to_server(RouteGuideServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+   
+if __name__ == '__main__':
+    serve()
---
+++ gRPC/final_pb2.pyi
@@ -0,0 +1,53 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class convertReply(_message.Message):
+    __slots__ = ["hash"]
+    HASH_FIELD_NUMBER: _ClassVar[int]
+    hash: str
+    def __init__(self, hash: _Optional[str] = ...) -> None: ...
+
+class convertRequest(_message.Message):
+    __slots__ = ["file"]
+    FILE_FIELD_NUMBER: _ClassVar[int]
+    file: bytes
+    def __init__(self, file: _Optional[bytes] = ...) -> None: ...
+
+class deleteReply(_message.Message):
+    __slots__ = ["result"]
+    RESULT_FIELD_NUMBER: _ClassVar[int]
+    result: int
+    def __init__(self, result: _Optional[int] = ...) -> None: ...
+
+class deleteRequest(_message.Message):
+    __slots__ = ["hash"]
+    HASH_FIELD_NUMBER: _ClassVar[int]
+    hash: str
+    def __init__(self, hash: _Optional[str] = ...) -> None: ...
+
+class downloadReply(_message.Message):
+    __slots__ = ["file"]
+    FILE_FIELD_NUMBER: _ClassVar[int]
+    file: bytes
+    def __init__(self, file: _Optional[bytes] = ...) -> None: ...
+
+class downloadRequest(_message.Message):
+    __slots__ = ["hash"]
+    HASH_FIELD_NUMBER: _ClassVar[int]
+    hash: str
+    def __init__(self, hash: _Optional[str] = ...) -> None: ...
+
+class queueReply(_message.Message):
+    __slots__ = ["file"]
+    FILE_FIELD_NUMBER: _ClassVar[int]
+    file: bytes
+    def __init__(self, file: _Optional[bytes] = ...) -> None: ...
+
+class queueRequest(_message.Message):
+    __slots__ = ["hash"]
+    HASH_FIELD_NUMBER: _ClassVar[int]
+    hash: str
+    def __init__(self, hash: _Optional[str] = ...) -> None: ...
